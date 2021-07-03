713568d68678ee4489478d531848fee63083685c
Emmanuel Kehinde <emmakoko1996@gmail.com>
2021-07-03 18:02:44 +0100

    Initial Commit

---
+++ ShazamClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShazamClone
+//
+//  Created by Emmanuel Kehinde on 03/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShazamClone/ShazamCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamCloneApp.swift
+//  ShazamClone
+//
+//  Created by Emmanuel Kehinde on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ShazamCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
